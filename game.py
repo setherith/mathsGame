1e3c161de186a92df71cd4d1741231e6a5f00352
Shane <setherith@hotmail.co.uk>
2016-09-21 19:36:11 +0100

    Multiplication, addition, subtraction and division asked with answers

--- game.py
+++ game.py
@@ -1,12 +1,40 @@
 import random
 
 for attempts in range(1, 11):
+
+    types_of_questions = ['subtraction', 'addition', 'multiplication', 'division']
+
+    question_type = types_of_questions[random.randint(0, 3)]
+
     a = random.randint(1, 12)
     b = random.randint(1, 12)
 
-    x = input('What is ' + str(a) + ' + ' + str(b) + ' = ? ')
-    if int(x) == a + b:
-        print('Correct!')
-    else:
-        print('Wrong!')
+    print(question_type)
 
+    if question_type == 'addition':
+        x = input('What is ' + str(a) + ' + ' + str(b) + ' = ? ')
+        if int(x) == a + b:
+            print('Correct!')
+        else:
+            print('Wrong!')
+
+    if question_type == 'subtraction':
+        x = input('What is ' + str(a) + ' - ' + str(b) + ' = ? ')
+        if int(x) == a - b:
+            print('Correct!')
+        else:
+            print('Wrong!')
+
+    if question_type == 'multiplication':
+        x = input('What is ' + str(a) + ' x ' + str(b) + ' = ? ')
+        if int(x) == a * b:
+            print('Correct!')
+        else:
+            print('Wrong!')
+
+    if question_type == 'division':
+        x = input('What is ' + str(a) + ' % ' + str(b) + ' = ? ')
+        if int(x) == a / b:
+            print('Correct!')
+        else:
+            print('Wrong!')
