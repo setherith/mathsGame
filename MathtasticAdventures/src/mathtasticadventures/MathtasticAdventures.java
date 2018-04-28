22e72a0ab94567ad91239cf911783d5edf0b0385
Shane Pudner <setherith@gmail.com>
2018-04-28 23:08:02 +0100

    Mucking about and making sure I can still build on my other machine

--- MathtasticAdventures/src/mathtasticadventures/MathtasticAdventures.java
+++ MathtasticAdventures/src/mathtasticadventures/MathtasticAdventures.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mathtasticadventures;
 
+import java.util.Scanner;
+
 /**
- *
  * @author setherith
  */
 public class MathtasticAdventures {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = reader.next();
+        System.out.println("Hello " + name);
+        reader.close();
     }
     
 }
